671d03d3cfb6a3fc0144adcaf4067d8a54369314
Dylan Morris <dylanmorris1506@gmail.com>
2026-06-23 13:00:13 +0100

    Add severity levels to brute force attack

--- src/detector.py
+++ src/detector.py
@@ -4,6 +4,10 @@ from datetime import datetime, timedelta
 
 def detect_bruteforce(entries):
     MAX_MINUTES = 5
+    MID_SEVERITY = 5
+    HIGH_SEVERITY = 10
+    CRITICAL_SEVERITY = 20
+    severity = None
     failed_logins = {}
     ip = None
     alerts = []
@@ -21,20 +25,29 @@ def detect_bruteforce(entries):
 
     # Loops through items in failed_logins
     for user, times in failed_logins.items():
-        # Checks if there are more than 5 timestampes logged
-        if len(times) >= 5:
-            window = times[4] - times[0]
+        fail_count = len(times)
 
-            # Checks if the window is within a 5 minute range
-            if window <= timedelta(minutes=MAX_MINUTES):
-                # Loops back through entries
-                for entry in entries:
-                    # Gets the ip address of the user with too many failed logins
-                    if entry.user == user:
-                        ip = entry.ip
-                        break
+        # Sets the severity level for the alert
+        if fail_count >= CRITICAL_SEVERITY:
+            severity = "Critical"
+        elif fail_count >= HIGH_SEVERITY:
+            severity = "High"
+        elif fail_count >= MID_SEVERITY:
+            severity = "Medium"
+        else:
+            continue
 
-                # Creates a new alert object
-                alerts.append(Alert("Bruteforce attack", "Low", user, ip))
+        window = times[4] - times[0]
+
+        # Checks if the window is greater than 5 minutes
+        if window <= timedelta(minutes=MAX_MINUTES):
+            for entry in entries:
+                # Gets the ip address of the user
+                if entry.user == user:
+                    ip = entry.ip
+                    break
+
+            # Creates a new alert object
+            alerts.append(Alert("Bruteforce attack", severity, user, ip))
 
     return alerts
